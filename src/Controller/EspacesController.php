df2ba38fa43d9dc4ddc45fb63a07f26c9202f301
Wexop <mattheurtin@gmail.com>
2022-12-08 16:19:49 +0100

    can change espaces

--- src/Controller/EspacesController.php
+++ src/Controller/EspacesController.php
@@ -26,16 +26,15 @@ class EspacesController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
 
-            if ( (($form->getData()->getDateOuverture() !== null && $form->getData()->getDateFermeture() !== null) && $form->getData()->getDateOuverture() < $form->getData()->getDateFermeture())
-                || ($form->getData()->getDateOuverture() === null && $form->getData()->getDateFermeture() === null )
+            if ((($form->getData()->getDateOuverture() !== null && $form->getData()->getDateFermeture() !== null) && $form->getData()->getDateOuverture() < $form->getData()->getDateFermeture())
+                || ($form->getData()->getDateOuverture() === null && $form->getData()->getDateFermeture() === null)
             ) {
                 $em = $doctrine->getManager();
 
                 $em->persist($espace);
 
                 $em->flush();
-            }
-            else {
+            } else {
                 throw $this->createNotFoundException("Erreur dans le formulaire : vérifier si les dates d'ouverture et de fermeture sont correctement indiquées");
             }
         }
@@ -52,37 +51,28 @@ class EspacesController extends AbstractController
     #[Route('/espaces/supprimer/{id}', name: 'espaces_supprimer')]
     public function supprimerEspace($id, ManagerRegistry $doctrine, Request $request)
     {
-        //Récupérer la catégorie dans la BDD
+
         $espace = $doctrine->getRepository(Espace::class)->find($id);
 
-        //si on n'a rien trouvé -> 404
 
         if (!$espace) {
             throw $this->createNotFoundException("Aucun espace avec l'id $id");
         }
 
-        //si on arrive la, c'est qu'on a trouvé une catégorie
-        //on crée le formulaire avec (il sera rempli avec ses valeurs)
         $form = $this->createForm(EspaceSupprimerType::class, $espace);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // le handleRequest a rempli notre objet $vcategorieNew
-            // qui n'est plus vide
-            //pour sauvegarder, on va récupérer un entitymanager de doctrine
-            //qui comme son nom l'indique gère les identités
+
             $em = $doctrine->getManager();
-            // on lui dit de la supprimer de la BDD
+
             $em->remove($espace);
 
-            //générer l'insert
             $em->flush();
 
-            //retour à la page d'accueil
             return $this->redirectToRoute("app_espaces");
 
         }
-
 
         return $this->render("espaces/supprimer.html.twig", [
             "espace" => $espace,
@@ -92,4 +82,45 @@ class EspacesController extends AbstractController
 
     }
 
+    #[Route('/espaces/modifier/{id}', name: 'espaces_modifier')]
+    public function modifierCategorie($id, ManagerRegistry $doctrine, Request $request)
+    {
+        $espace = $doctrine->getRepository(Espace::class)->find($id);
+
+        if (!$espace) {
+            throw $this->createNotFoundException("Aucune espace avec l'id $id");
+        }
+
+        $form = $this->createForm(EspaceType::class, $espace);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            if ((($form->getData()->getDateOuverture() !== null && $form->getData()->getDateFermeture() !== null) && $form->getData()->getDateOuverture() < $form->getData()->getDateFermeture())
+                || ($form->getData()->getDateOuverture() === null && $form->getData()->getDateFermeture() === null)
+            ) {
+
+
+                $em = $doctrine->getManager();
+
+                $em->persist($espace);
+
+
+                $em->flush();
+
+                return $this->redirectToRoute("app_espaces");
+
+            } else {
+                throw $this->createNotFoundException("Erreur dans le formulaire : vérifier si les dates d'ouverture et de fermeture sont correctement indiquées");
+            }
+        }
+
+
+            return $this->render("espaces/modifier.html.twig", [
+                "espace" => $espace,
+                "formulaire" => $form->createView()
+            ]);
+
+
+        }
 }
