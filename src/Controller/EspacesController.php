6e012cb52a29c024dca4d4384d09d27e1a787373
Wexop <mattheurtin@gmail.com>
2022-12-08 16:06:42 +0100

    can add espace only if dates are fine

--- src/Controller/EspacesController.php
+++ src/Controller/EspacesController.php
@@ -2,17 +2,52 @@
 
 namespace App\Controller;
 
+use App\Entity\Espace;
+use App\Form\EspaceType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function PHPUnit\Framework\throwException;
 
 class EspacesController extends AbstractController
 {
     #[Route('/', name: 'app_espaces')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine, Request $request): Response
     {
+
+        $espace = new Espace();
+        $form = $this->createForm(EspaceType::class, $espace);
+
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            if ( (($form->getData()->getDateOuverture() !== null && $form->getData()->getDateFermeture() !== null) && $form->getData()->getDateOuverture() < $form->getData()->getDateFermeture())
+                || ($form->getData()->getDateOuverture() === null && $form->getData()->getDateFermeture() === null )
+            ) {
+                $em = $doctrine->getManager();
+
+                $em->persist($espace);
+
+                $em->flush();
+            }
+            else {
+                throw $this->createNotFoundException("Erreur dans le formulaire : vérifier si les dates d'ouverture et de fermeture sont correctement indiquées");
+            }
+
+
+
+        }
+
+        $repo = $doctrine->getRepository(Espace::class);
+        $espace = $repo->findAll();
+
         return $this->render('espaces/index.html.twig', [
-            'controller_name' => 'EspacesController',
+            "espaces" => $espace,
+            "formulaire" => $form->createView()
         ]);
     }
 }
---
+++ src/Form/EspaceType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Espace;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EspaceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('superficie')
+            ->add('date_ouverture')
+            ->add('date_fermeture')
+            ->add("OK", SubmitType::class, ["label" => "Ajouter"])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Espace::class,
+        ]);
+    }
+}
